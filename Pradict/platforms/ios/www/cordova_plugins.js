742e3d245835c3c3590bfa6319634a55891a72ba
piyali71 <piyali.pandit@vrisini.com>
2016-04-07 13:25:54 +0530

    1st
    commit

---
+++ Pradict/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,37 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/com.microsoft.azure-mobile-services/www/MobileServices.Web.Ext.js",
+        "id": "com.microsoft.azure-mobile-services.AzureMobileServices.Ext",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.microsoft.azure-mobile-services/www/MobileServices.Web.js",
+        "id": "com.microsoft.azure-mobile-services.AzureMobileServices",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "com.phonegap.plugins.PushPlugin": "2.4.0",
+    "com.microsoft.azure-mobile-services": "1.2.9",
+    "org.apache.cordova.inappbrowser": "0.6.0"
+}
+// BOTTOM OF METADATA
+});
